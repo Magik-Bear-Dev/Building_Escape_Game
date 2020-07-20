25b7b26f7eafbc504a2bb8625bfa10bc4804a17b
marksappstudio <marksappstudio>
2020-07-20 16:26:35 +1000

    S4, Lecture 104 - Exposing Parameters To The Editor

--- Source/Building_Escape_2/OpenDoor.cpp
+++ Source/Building_Escape_2/OpenDoor.cpp
@@ -21,7 +21,7 @@ void UOpenDoor::BeginPlay()
 	Super::BeginPlay();
 	InitialYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitialYaw;
-	TargetYaw = InitialYaw + 90.f;
+	TargetYaw += InitialYaw; // TargetYaw = TargetYaw + InitialYaw;
 }
 
 
@@ -37,11 +37,6 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	FRotator DoorRotation = GetOwner()->GetActorRotation();
 	DoorRotation.Yaw = CurrentYaw;
 	GetOwner()->SetActorRotation(DoorRotation);
-
-	// float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
-	// FRotator OpenDoor(0.f, TargetYaw, 0.f);
-	// OpenDoor.Yaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2);
-	// GetOwner()->SetActorRotation(OpenDoor);
 }
 
 
